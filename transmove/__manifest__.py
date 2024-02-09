61e1fa2549d01f729f5f7c0cc6ada027cbee3f31
jayp-odoo <jayp@odoo.com>
2024-02-09 18:07:39 +0530

    ch6 half-menus_views

--- transmove/__manifest__.py
+++ transmove/__manifest__.py
@@ -1,10 +1,12 @@
 {
-    'name':'transmove',
-    'depends':[
-        'base'
+    "name": "transmove",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/transmove_quotations_views.xml",
+        "views/transmove_menus.xml",
     ],
-    'application':True,
-    'installable':True,
-    'auto_install':True,
-
+    "application": True,
+    "installable": True,
+    "auto_install": True,
 }
